e24ede955b14d7f241d5f385eba8f3e3cab0867c
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-02-14 01:27:44 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -21,34 +21,34 @@ int main() {
 		// store all the possible moves of every knight in a set
 		for (int i = 0; i < n; i++) {
  			s.insert((x[i] + 1) + (y[i] + 2));
-  		s.insert((x[i] - 1) + (y[i] + 2));
-  		s.insert((x[i] + 2) + (y[i] + 1));
-  		s.insert((x[i] + 2) + (y[i] - 1));
-  		s.insert((x[i] + 1) + (y[i] - 2));
-  		s.insert((x[i] - 1) + (y[i] - 2));
-  		s.insert((x[i] - 2) + (y[i] + 1));
-  		s.insert((x[i] - 2) + (y[i] - 1));
+			s.insert((x[i] - 1) + (y[i] + 2));
+			s.insert((x[i] + 2) + (y[i] + 1));
+			s.insert((x[i] + 2) + (y[i] - 1));
+			s.insert((x[i] + 1) + (y[i] - 2));
+			s.insert((x[i] - 1) + (y[i] - 2));
+			s.insert((x[i] - 2) + (y[i] + 1));
+			s.insert((x[i] - 2) + (y[i] - 1));
+		}
+		int flag = 0;
+		// check if the original position of the king is in check
+		// also, check if all the possible moves of the king are blocked by the knights
+		// if the king cannot move at all, then it's considered as checkmate
+		if (s.find(a + b) != s.end()) {
+			flag++;
+		} 
+		if (s.find(a + (b + 1)) != s.end()) {
+			flag++;
+		}
+		if (s.find((a + 1) + b) != s.end()) {
+			flag++; 
+		}
+		if (s.find(a + (b - 1)) != s.end()) {
+			flag++;
+		}
+		if (s.find((a - 1) + b) != s.end()) {	
+			flag++;
+		}
+		cout << (flag == 5 ? "YES" : "NO") << '\n';	
   	}
-   	int flag = 0;
-   	// check if the original position of the king is in check
-   	// also, check if all the possible moves of the king are blocked by the knights
-   	// if the king cannot move at all, then it's considered as checkmate
-   	if (s.find(a + b) != s.end()) {
- 			flag++;
- 		} 
- 		if (s.find(a + (b + 1)) != s.end()) {
- 			flag++;
- 		}
- 		if (s.find((a + 1) + b) != s.end()) {
- 			flag++; 
- 		}
- 		if (s.find(a + (b - 1)) != s.end()) {
- 			flag++;
- 		}
- 		if (s.find((a - 1) + b) != s.end()) {	
- 			flag++;
- 		}
- 		cout << (flag == 5 ? "YES" : "NO") << '\n';	
-  }
 	return 0;
 }
